83af658ada96f56391cfb4f0e4d65a59314dd99e
Jesse Dubay <jesse@thefortytwo.net>
2011-04-25 22:38:55 -0700

    Add month/year to by_slug URL route so I don't break my permalinks.

--- wake/views.py
+++ wake/views.py
@@ -11,12 +11,16 @@ store = CouchStore().load()
 def wake():
     return render_template('stream.html', events=store.collapsed_events())
 
-@app.route('/<slug>')
-def by_slug(slug):
+@app.route('/<year>/<month>/<slug>')
+def by_slug(year, month, slug):
     events = list(store.events_by_slug(slug))
     if not events:
         abort(404)
-    return render_template('single.html', event=events[0])
+    event = events[0]
+    timestamp = datetime.fromtimestamp(event['timestamp'])
+    if not timestamp.strftime('%Y') == year or not timestamp.strftime('%m') == month:
+        abort(404)
+    return render_template('single.html', event=event)
 
 @app.route('/recent.atom')
 def recent_feed():
@@ -25,11 +29,15 @@ def recent_feed():
     sources = store.get_sources()
     for event in store.events():
         if sources[event['source']].get('syndicate'):
+            timestamp = datetime.fromtimestamp(event['timestamp'])
+            year = timestamp.strftime('%Y')
+            month = timestamp.strftime('%m')
+            url = url_for('by_slug', year=year, month=month, slug=event['slug'])
             feed.add(event['title'],
                      unicode(event['content']),
                      content_type='html',
                      author=event.get('author', ''),
-                     url=urljoin(request.url_root, url_for('by_slug', slug=event.get('slug', ''))),
+                     url=urljoin(request.url_root, url),
                      updated=datetime.fromtimestamp(event['timestamp']),
                      published=datetime.fromtimestamp(event['timestamp']))
     return feed.get_response()
