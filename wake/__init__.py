cbd28dc51f74f5706768b8cac339749b264ac986
Jesse Dubay <jesse@thefortytwo.net>
2011-05-12 20:35:06 -0700

    Add templates and multis for all kinds.

--- wake/__init__.py
+++ wake/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from urllib import quote_plus
 from wake.filters import relative_time, markup_tweet
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.filters["relativetime"] = relative_time
 app.jinja_env.filters["tweet"] = markup_tweet
+app.jinja_env.filters["quoteplus"] = lambda s: quote_plus(s.encode('utf8'))
 
 import wake.views
